41fc49445d156f3f1c0fba74ba8d0b18ae2a7bd8
DanielJBailey <Dbailey0210@gmail.com>
2018-11-08 12:28:50 -0700

    add notes

--- main.rb
+++ main.rb
@@ -5,3 +5,26 @@ end
 def another_method
     puts "Hi Again"
 end
+
+
+
+# git fetch origin master
+# updates if other contributor updates something. pulls in changes
+# FETCH CHANGES
+
+
+# APPLY CHANGES
+# git rebase origin/master
+
+
+
+# if you dont want to commit but you need someone else's changes run
+# git stash
+# stash is an array of changes
+# then git status
+
+# git fetch origin master
+# git rebase origin/master
+
+#git stash apply
+#git status
